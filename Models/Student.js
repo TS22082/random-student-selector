4ae42722561e6e5f8b767483a130fed6a93ace8d
Brian Hicks <brian.david.hicks@gmail.com>
2020-03-30 22:15:38 -0600

    Added linting and able to view remaining students

--- Models/Student.js
+++ Models/Student.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
 const StudentSchema = new Schema({
   name: {
     type: String,
     trim: true,
-    required: "Student name is required"
-  }
+    required: 'Student name is required',
+  },
 });
 
-const Student = mongoose.model("Student", StudentSchema);
+const Student = mongoose.model('Student', StudentSchema);
 
 module.exports = Student;
